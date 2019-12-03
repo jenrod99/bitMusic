d7bf0f65c3cd600a41cc7858e6aefa18b2e0335c
ramones2379 <dxcami@gmail.com>
2019-12-02 20:27:35 -0500

    Lógica de login

--- bit-music/src/app/componentes/login/login.component.ts
+++ bit-music/src/app/componentes/login/login.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Usuario } from '../../modelos/usuario';
+import { UsuarioService } from 'src/app/servicios/usuario.service';
+import { Router } from '@angular/router';
+import { CompartidoService } from 'src/app/servicios/compartido.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+  usuario: Usuario;
+  loginCorrecto;
 
-  constructor() { }
+  constructor(
+    private _usuarioService: UsuarioService,
+    private _router: Router,
+    private _compartidoService: CompartidoService
+  ) {
+    if(localStorage.getItem("sesion") != null) {
+      this._router.navigate(['/menu']);
+    }
+    this.usuario = new Usuario ('', '', null, '', '', '', '')
+  }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy () {
+    
   }
 
 }
