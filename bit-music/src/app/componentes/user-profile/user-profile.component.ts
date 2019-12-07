b4cfd1ed3218c99c8bb881824378524ffca5a87e
chiano-co <dxcami@gmail.com>
2019-12-06 21:20:42 -0500

    Cambios de cerrar sesión. Tienen un error :(

--- bit-music/src/app/componentes/user-profile/user-profile.component.ts
+++ bit-music/src/app/componentes/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from './../../../app/modelos/usuario'
 import { UsuarioService } from 'src/app/servicios/usuario.service';
 
 @Component({
@@ -7,29 +8,76 @@ import { UsuarioService } from 'src/app/servicios/usuario.service';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-  name='';
+
+  usuario: Usuario;
+  filesToUpload: File;
+  actualizacionCorrecta;
+  url = "http://localhost:3977/api/";
+
+  /* name='';
   age='';
   email='';
   password='';
 
-  title='';
+  title=''; */
 
   constructor(
     private _usuarioService: UsuarioService,
   ) {
-    this.getUser();
+    /* this.getUser(); */
    }
 
-  ngOnInit() {
+   ngOnInit() {
+    this.usuario = JSON.parse(localStorage.getItem("sesion"))
+    console.log(this.usuario)
   }
 
-  getUser(){
+  actualizarDatos(){
+    this._usuarioService.actualizarUsuario(this.usuario._id,this.usuario).subscribe(
+      (response:any)=>{
+        if(response.usuario){
+          if(this.filesToUpload != undefined){
+              this._usuarioService.cargarImagenUsuario(this.filesToUpload,this.usuario._id)
+              .subscribe(
+                (response:any)=>{
+                  if(response.usuario){
+                    this.actualizacionCorrecta = "Datos actualizados correctamente";
+                    this.usuario = response.usuario;
+                    localStorage.setItem("sesion",JSON.stringify(this.usuario));
+                  }else{
+                    this.actualizacionCorrecta = "Los datos no se actualizaron por completo. Contacta al administrador de la aplicacion";
+                  }
+                },error=>{
+                  if (error != null) {
+                    console.log(error)
+                  }
+                }
+              )
+          };
+          localStorage.setItem("sesion",JSON.stringify(this.usuario));
+        }else{
+          this.actualizacionCorrecta = "No se han podido actualizar sus datos, comuniquese con el administrador de la aplicacion";
+        }
+      },error=>{
+        if (error != null) {
+          console.log(error)
+        }
+      }
+    )
+  }  fileChangeEvent(fileInput: any) {
+    this.filesToUpload = <File>fileInput.target.files[0];//recoger archivos seleccionados en el input
+  }}
+
+
+  // Anterior
+
+  /* getUser(){
     if(this._usuarioService.usuario){
       this.name = this._usuarioService.usuario.nombre? this._usuarioService.usuario.nombre : '';
       this.age = this._usuarioService.usuario.edad? this._usuarioService.usuario.edad : '';
       this.email = this._usuarioService.usuario.correo? this._usuarioService.usuario.correo : '';
       this.password = this._usuarioService.usuario.password? this._usuarioService.usuario.password : '';
     }
-  }
+  } */
 
-}
+
